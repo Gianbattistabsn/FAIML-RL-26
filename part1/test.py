06c7afbf77a2574d0d7d54c528e477b6c3a3cfb0
Gianbattista Busonera <gianbattista.busonera@studenti.polito.it>
2026-05-12 09:47:03 +0200

    baseline = 20

--- part1/test.py
+++ part1/test.py
@@ -76,7 +76,7 @@ def main():
     print(ckpts)
 
     ckpt = ckpts[-1]
-    ckpt = "part1/checkpoints/reinforce_baseline_-1/reinforce_best_ep11000_avg661.pt"
+    ckpt = "part1/checkpoints/reinforce_baseline_-1/reinforce_best_ep25000_avg941.pt"
     print(f"Loading: {ckpt}")
     rewards = evaluate(ckpt, render=RENDER)
     print(f"Mean reward: {np.mean(rewards):.2f}")
